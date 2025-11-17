bf3f382019745f3bdaaac6c9ae4d053716d618e0
Manika7131 <131638059+Manika7131@users.noreply.github.com>
2025-11-17 22:12:49 +0530

    Fix formatting of WhatsAppButton component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <WhatsAppButton />
+      <WhatsAppButton/>
       <CallButton/>
 
     
